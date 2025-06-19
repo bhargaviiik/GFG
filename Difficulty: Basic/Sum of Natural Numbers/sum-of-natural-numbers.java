5758c37e8533845975dda9243c790e8697c53699
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-06-19 13:46:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public static int seriesSum(int n) {
+        // code here
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+}
