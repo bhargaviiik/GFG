89d78092d033771c9b4bb65bc562b17a5aaca9e0
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-01-10 13:34:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.java
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -1,9 +1,12 @@
 class Solution {
     static boolean isPrime(int n) {
         // code here
+        //Instead of checking till n, we can check till √n because a larger factor of n must be a multiple of a smaller factor that has been already checked.
         if(n==1) return false;
-        for(int i=2;i<n;i++){
-            if(n%i==0) return false;
+        if(n==2 || n==3 || n==5) return true;
+        if(n%2==0 || n%3==0) return false;
+        for(int i=6;(i-1)<=(int)Math.sqrt(n);i=i+6){
+            if(n%(i-1)==0 || n%(i+1)==0) return false;
         }
         return true;
     }
